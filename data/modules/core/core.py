1020cfb91c03b438ad220627287f62fcc6fe258a
Kicend <kicend@gmail.com>
2020-02-25 21:29:55 +0100

    Poprawka funkcji startup

--- data/modules/core/core.py
+++ data/modules/core/core.py
@@ -2,18 +2,20 @@ import json
 import os
 
 # Stałe dla RoleSU
-SERVERS_PREFIXES_FILE = "data/settings/servers_prefixes/prefixes.json"
+SERVERS_PREFIXES_DIR = "data/settings/servers_prefixes"
 SERVERS_SETTINGS_FILES = "data/settings/servers_prefixes"
 DEFAULT_SERVER_PREFIX = "!"
 
-cache = {"waiting_messages": {}}
+cache = {"waiting_messages": {}, "server_parameters": {}}
 guild_list = []
 
 async def startup(bot):
     for guild in bot.guilds:
         guild_list.append(str(guild.id))
-    if not os.path.isfile(SERVERS_PREFIXES_FILE):
-        with open(SERVERS_PREFIXES_FILE, "a") as f:
+    os.makedirs(SERVERS_PREFIXES_DIR, exist_ok=True)
+    os.makedirs(SERVERS_SETTINGS_FILES, exist_ok=True)
+    if not os.path.isfile(SERVERS_PREFIXES_DIR + "/prefixes.json"):
+        with open(SERVERS_PREFIXES_DIR + "/prefixes.json", "a") as f:
             servers_prefixes = {}
             for guild in guild_list:
                 servers_prefixes[guild] = DEFAULT_SERVER_PREFIX
@@ -23,17 +25,12 @@ class GuildParameters:
     def __init__(self, guild_id):
         self.id = guild_id
         self.settings_filename = SERVERS_SETTINGS_FILES + "/{}.json".format(self.id)
-        self.prefixes_filename = SERVERS_PREFIXES_FILE
+        self.prefixes_filename = SERVERS_PREFIXES_DIR + "/prefixes.json"
 
     async def get_prefix(self):
-        if cache["server_prefix"]:
-            prefixes = list(cache["servers_prefixes"])
+        with open(self.prefixes_filename, "r") as f:
+            prefixes = json.load(f)
             server_prefix = prefixes[str(self.id)]
-        else:
-            with open(self.prefixes_filename, "r") as f:
-                prefixes = json.load(f)
-                cache["servers_prefixes"] = prefixes
-                server_prefix = prefixes[str(self.id)]
 
         return server_prefix
 
@@ -44,4 +41,8 @@ class GuildParameters:
             with open(self.prefixes_filename, "w") as f:
                 prefixes[str(self.id)] = prefix
                 json.dump(prefixes, f, indent=4)
-                await ctx.send("Prefix został pomyślnie zmieniony na '{}'".format(prefix))
+        else:
+            with open(self.prefixes_filename, "a") as f:
+                prefixes = {str(ctx.guild.id): prefix}
+                json.dump(prefixes, f, indent=4)
+        await ctx.send("Prefix został pomyślnie zmieniony na '{}'".format(prefix))
